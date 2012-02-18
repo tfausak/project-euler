868add7098652646edba79ec16f8535091899936
Taylor Fausak <taylor@fausak.me>
2012-02-18 15:42:21 -0600

    Remove problem definitions.
    The link to Project Euler's problem definition should be enough.

--- php/pe001.php
+++ php/pe001.php
@@ -1,12 +1,6 @@
 <?php
 
-/* http://projecteuler.net/problem=1
-
-If we list all the natural numbers below 10 that are multiples of
-3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+# http://projecteuler.net/problem=1
 
 $sum = 0;
 for ($n = 0; $n < 1000; $n += 1) {
