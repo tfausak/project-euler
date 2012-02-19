ee0dab7437acfb05aee9ea14a1db04294552a1d6
Taylor Fausak <taylor@fausak.me>
2012-02-18 18:12:36 -0600

    Clean up Haskell solution.

--- haskell/pe001.hs
+++ haskell/pe001.hs
@@ -1,3 +1,4 @@
 -- http://projecteuler.net/problem=1
-main = putStrLn (show (sum
-    [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]))
+
+main = do
+    putStrLn . show $ sum [n | n <- [1 .. 999], rem n 3 == 0 || rem n 5 == 0]
