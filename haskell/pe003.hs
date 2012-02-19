5891c17ef2ac737d12a121f50d544e1150043473
Taylor Fausak <taylor@fausak.me>
2012-02-18 18:53:57 -0600

    Add Haskell solution to problem 3.

---
+++ haskell/pe003.hs
@@ -0,0 +1,13 @@
+-- http://projecteuler.net/problem=3
+
+bound :: Int -> Int
+bound n = floor . sqrt . fromIntegral $ n
+
+prime :: Int -> Bool
+prime n = [d | d <- [1 .. bound n], rem n d == 0] == [1]
+
+factors :: Int -> [Int]
+factors n = [d | d <- [1 .. bound n], rem n d == 0, prime d]
+
+main = do
+    putStrLn . show . maximum . factors $ 600851475143
