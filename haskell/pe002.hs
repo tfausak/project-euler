709123ed36a58bacde160b25cd2b6e06dc8f2265
Taylor Fausak <taylor@fausak.me>
2012-02-18 18:19:24 -0600

    Clean up Haskell solution.

--- haskell/pe002.hs
+++ haskell/pe002.hs
@@ -1,10 +1,11 @@
 -- http://projecteuler.net/problem=2
 
 fibonacci :: Int -> Int
-fibonacci 0 = 0
-fibonacci 1 = 1
-fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+fibonacci n
+    | n == 0 = 0
+    | n == 1 = 1
+    | otherwise = fibonacci (n - 1) + fibonacci (n - 2)
 
 main = do
-    putStrLn (show (sum (takeWhile (< 4000000)
-        [n | n <- [fibonacci n | n <- [1 ..]], even n])))
+    putStrLn . show $ sum (takeWhile
+        (< 4000000) [n | n <- [fibonacci n | n <- [1 ..]], even n])
