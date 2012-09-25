58479f81323df11c005bb843b5a7ae6c2f6b96ce
Taylor Fausak <taylor@fausak.me>
2012-09-25 17:51:45 -0500

    Write a more idiomatic Ruby solution to #1.

--- ruby/pe001.rb
+++ ruby/pe001.rb
@@ -1,8 +1,2 @@
 # http://projecteuler.net/problem=1
-sum = 0
-for n in 0..999
-    if n % 3 == 0 or n % 5 == 0
-        sum += n
-    end
-end
-puts sum
+puts (0..999).select { |n| n % 3 == 0 || n % 5 == 0 }.inject{ |sum, x| sum + x }
