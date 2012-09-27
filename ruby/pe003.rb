d2cd6d21bcdeb869f7b77aab03cdf45b5d788e33
Taylor Fausak <taylor@fausak.me>
2012-09-26 22:14:56 -0500

    Add Ruby solution to problem 3.

---
+++ ruby/pe003.rb
@@ -0,0 +1,12 @@
+# http://projecteuler.net/problem=3
+n = 600_851_475_143
+n /= 2 while n % 2 == 0
+
+factor = 3
+while n != 1
+    n /= factor while n % factor == 0
+    factor += 2
+end
+factor -= 2
+
+puts factor
