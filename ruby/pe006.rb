62204c1a46d49bbf10914d8148981b36f6b96288
Taylor Fausak <taylor@fausak.me>
2012-09-27 14:19:05 -0500

    Add Ruby solution to problem 6.

---
+++ ruby/pe006.rb
@@ -0,0 +1,6 @@
+# http://projecteuler.net/problem=6
+limit = 100
+sum_of_squares = (1..limit).inject { |sum, n| sum + n ** 2 }
+square_of_sum = (1..limit).inject { |sum, n| sum + n } ** 2
+difference = (sum_of_squares - square_of_sum).abs
+puts difference
