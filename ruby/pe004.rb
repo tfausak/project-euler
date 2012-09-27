89113802b08d3c24550c8f1ef3ff813611785237
Taylor Fausak <taylor@fausak.me>
2012-09-26 22:22:44 -0500

    Add Ruby solution to problem 4.

---
+++ ruby/pe004.rb
@@ -0,0 +1,10 @@
+# http://projecteuler.net/problem=4
+max = 0
+999.downto(900).each do |x|
+    x.downto(900).each do |y|
+        product = x * y
+        break if product < max
+        max = product if product.to_s == product.to_s.reverse
+    end
+end
+puts max
