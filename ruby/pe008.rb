0720d8c38ab5a3a746b4436d45d9b2eee56988f2
Taylor Fausak <taylor@fausak.me>
2012-09-27 15:10:07 -0500

    Add Ruby solution to problem 8.

---
+++ ruby/pe008.rb
@@ -0,0 +1,9 @@
+# http://projecteuler.net/problem=8
+number = File.read(ARGV[0]).inject { |number, line| number += line.chomp }
+max = 0
+(0..number.length - 5).each do |offset|
+  digits = number[offset..offset + 4].chars.collect { |digit| digit.to_i }
+  product = digits.inject { |product, digit| product *= digit }
+  max = product if product > max
+end
+puts max
