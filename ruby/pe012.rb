bb0cc47602d8536cdf4289387a95d01c65cea9b2
Taylor Fausak <taylor@fausak.me>
2012-10-01 14:08:49 -0500

    Add Ruby solution to problem 12.

---
+++ ruby/pe012.rb
@@ -0,0 +1,21 @@
+# http://projecteuler.net/problem=12
+def divisors (n)
+  result = 2
+  (2..Math.sqrt(n)).each do |d|
+    if n % d == 0
+      result += 2
+    end
+  end
+  result
+end
+
+limit = 500
+triangle = 0
+n = 1
+while true
+  triangle = (n * (n + 1)) / 2
+  d = divisors(triangle)
+  break if d > limit
+  n += 1
+end
+puts triangle
