0dcc46e90297ebf847daf0558638997e74bdb72d
Taylor Fausak <taylor@fausak.me>
2012-10-01 16:54:38 -0500

    Add Ruby solution to problem 19.

---
+++ ruby/pe019.rb
@@ -0,0 +1,10 @@
+# http://projecteuler.net/problem=19
+require 'date'
+
+sundays = 0
+(1901..2000).each do |year|
+  (1..12).each do |month|
+    sundays += 1 if Date.new(year, month, 1).jd % 7 == 6
+  end
+end
+puts sundays
