90af81f5f27d996eb6564da1838dd248e7b16737
Taylor Fausak <taylor@fausak.me>
2012-10-04 17:22:30 -0500

    Simplify loop.
    It checks more trial divisors, but it doesn't slow the program down
    enough to matter.

--- ruby/pe003.rb
+++ ruby/pe003.rb
@@ -1,13 +1,11 @@
 #!/usr/bin/env ruby
 # http://projecteuler.net/problem=3
-n = 600_851_475_143
-n /= 2 while n % 2 == 0
 
-factor = 3
+n = 600_851_475_143
+factor = 1
 while n != 1
+  factor += 1
   n /= factor while n % factor == 0
-  factor += 2
 end
-factor -= 2
 
 puts factor
