9453c7a44ef9c55ce725d13612cea4f8b2999b02
Taylor Fausak <taylor@fausak.me>
2012-10-04 16:31:57 -0500

    Add Ruby solution to problem 21.
    That is some dense code.

---
+++ ruby/pe021.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# http://projecteuler.net/problem=21
+
+def aliquot_sum(n)
+  sqrt = Math.sqrt(n)
+  return (2..sqrt).select { |d| n % d == 0 }.reduce(n == 1 ? 0 : 1) do |sum, d|
+    sum + d + (d == sqrt ? 0 : n / d)
+  end
+end
+
+aliquot_sums = Hash[(1..10000).map { |n| [n, aliquot_sum(n)] }]
+aliquot_sums.reject! { |n, sum| n == sum || aliquot_sums[sum] != n }
+puts aliquot_sums.keys.reduce(:+)
