27e07e7fe4466bb0c7f24c47beab202d70c375fa
Taylor Fausak <taylor@fausak.me>
2012-10-05 16:31:26 -0500

    Simplify mapping using Symbol#to_proc.

--- ruby/pe013.rb
+++ ruby/pe013.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # http://projecteuler.net/problem=13
 
-puts File.read(ARGV[0]).map { |line| line.to_i }.reduce(:+).to_s.slice(0, 10)
+puts File.read(ARGV[0]).map(&:to_i).reduce(:+).to_s.slice(0, 10)
