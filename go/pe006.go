fee1ba31446e5fea466f1c5e97c276cb88019bd4
Taylor Fausak <taylor@fausak.me>
2013-04-14 15:20:23 -0500

    Add a Go solution to problem 6.

---
+++ go/pe006.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	sum_of_squares := 0
+	square_of_sums := 0
+	for n := 1; n <= 100; n++ {
+		sum_of_squares += n * n
+		square_of_sums += n
+	}
+	square_of_sums *= square_of_sums
+	fmt.Println(square_of_sums - sum_of_squares)
+}
