61e1a11ba9586172a17256c24bf6a32fcd19a3a8
Taylor Fausak <taylor@fausak.me>
2013-04-17 23:08:46 -0500

    Add a Go solution to problem 11.

---
+++ go/pe011.go
@@ -0,0 +1,79 @@
+// http://projecteuler.net/problem=11
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/big"
+	"os"
+)
+
+func main() {
+	// Read the file into an array of bytes.
+	bytes, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	// Splits the array of bytes into a grid of integers.
+	grid := [][]int{{-1}}
+	row := 0
+	column := 0
+	for _, byte := range bytes {
+		if byte == 10 {
+			grid = append(grid, []int{-1})
+			row += 1
+			column = 0
+		} else if byte == 32 {
+			grid[row] = append(grid[row], -1)
+			column += 1
+		} else if byte >= 48 && byte <= 57 {
+			digit := int(byte - 48)
+			if grid[row][column] == -1 {
+				grid[row][column] = digit * 10
+			} else {
+				grid[row][column] += digit
+			}
+		}
+	}
+	grid = grid[0 : len(grid)-1]
+
+	// Find the greatest product.
+	max := big.NewInt(0)
+	size := 4
+	for row := 0; row < len(grid); row++ {
+		for column := 0; column < len(grid[row]); column++ {
+			if row+size > len(grid) || column+size > len(grid[row]) || column < size {
+				continue
+			}
+
+			product_a := big.NewInt(1)
+			product_b := big.NewInt(1)
+			product_c := big.NewInt(1)
+			product_d := big.NewInt(1)
+
+			// Search right, down, down-right, and down-left.
+			for offset := 0; offset < size; offset++ {
+				product_a.Mul(product_a, big.NewInt(int64(grid[row][column+offset])))
+				product_b.Mul(product_b, big.NewInt(int64(grid[row+offset][column])))
+				product_c.Mul(product_c, big.NewInt(int64(grid[row+offset][column+offset])))
+				product_d.Mul(product_d, big.NewInt(int64(grid[row+offset][column-offset])))
+			}
+
+			if product_a.Cmp(max) > 0 {
+				max = product_a
+			}
+			if product_b.Cmp(max) > 0 {
+				max = product_b
+			}
+			if product_c.Cmp(max) > 0 {
+				max = product_c
+			}
+			if product_d.Cmp(max) > 0 {
+				max = product_d
+			}
+		}
+	}
+
+	fmt.Println(max)
+}
