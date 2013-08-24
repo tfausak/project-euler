e8374dca42679c0246d201c4abe19e8c5fa58dc4
Taylor Fausak <taylor@fausak.me>
2013-08-24 18:17:11 -0500

    Speed up Haskell solution to problem 9

--- haskell/pe009.hs
+++ haskell/pe009.hs
@@ -1,9 +1,8 @@
 -- http://projecteuler.net/problem=9
 
-main = print [(a, b, c) |
-        a <- [1..997],
-        b <- [a + 1..997],
-        c <- [b + 1..997],
-        a + b + c == 1000,
+main = print $ maximum $ map product [[a, b, c] |
+        a <- [1..332], -- 332 + 333 + 334 = 999
+        b <- [a + 1..1000 - a], -- a + b <= 1000
+        c <- [1000 - b - a],
         a ^ 2 + b ^ 2 == c ^ 2
     ]
